f981595d1fafb532dae9c8ae3077c3e23bff907e
김지훈 <kigpand@gmail.com>
2026-02-05 22:07:55 +0900

    refactor: error nomalize 작업

--- BaseSchema.ts
+++ BaseSchema.ts
@@ -1,20 +1,27 @@
-import { ISchema, SafeParseResult } from "./interface/ISchema";
+import { ISchema, SafeParseResult, ValidationError } from "./interface/ISchema";
 
 export abstract class BaseSchema<T> implements ISchema<T> {
   abstract parse(input: unknown): T;
 
+  protected normalizeError(e: unknown): ValidationError[] {
+    if (Array.isArray(e)) {
+      return e;
+    }
+
+    if (e instanceof Error) {
+      return [{ message: e.message }];
+    }
+
+    return [{ message: "Invalid value" }];
+  }
+
   safeParse(input: unknown): SafeParseResult<T> {
     try {
-      const data = this.parse(input);
-      return { success: true, data };
+      return { success: true, data: this.parse(input) };
     } catch (e) {
       return {
         success: false,
-        errors: [
-          {
-            message: e instanceof Error ? e.message : "Invalid value",
-          },
-        ],
+        errors: this.normalizeError(e),
       };
     }
   }
